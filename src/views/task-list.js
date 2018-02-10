5f564d2d4493a5fe7888e35b8400e8cbb055ba27
Avalander <darth.quesorico@gmail.com>
2018-02-10 11:25:37 +0100

    Add new task.

--- src/views/task-list.js
+++ src/views/task-list.js
@@ -1,6 +1,7 @@
 import './task-list.scss'
 
 import xs from 'xstream'
+import sampleCombine from 'xstream/extra/sampleCombine'
 
 import {
 	div,
@@ -12,30 +13,39 @@ import {
 import isolate from '@cycle/isolate'
 
 
-const view = () => xs.of([
-	div('.column', [
-		div('.row', [
-			span('Twilight Sparkle'),
+const ponies = [
+	'Twilight Sparkle',
+	'Fluttershy',
+	'Rainbow Dash',
+]
+const view = (tasks$, new_task_text$) => xs.combine(tasks$, new_task_text$)
+	.map(([tasks, text]) => [
+		div('.column', tasks.map(x => div('.row', [
+			span(x),
 			input({ props: { type: 'checkbox' }}),
-		]),
-		div('.row', [
-			span('Fluttershy'),
-			input({ props: { type: 'checkbox' }}),
-		]),
-		div('.row', [
-			span('Rainbow Dash'),
-			input({ props: { type: 'checkbox' }}),
-		]),
-	]),
-	div('.bottom-bar', [
-		input('.dark', { props: { placeholder: 'Text' }}),
-		button(i('.fa.fa-plus'))
+		]))),
+		div('.bottom-bar', [
+			input('.new-task.dark', { props: { placeholder: 'Text', value: text }}),
+			button('.add-task', i('.fa.fa-plus'))
+		])
 	])
-])
 
-export default sources => isolate(({ DOM }) => {
+export default /*sources => isolate(*/({ DOM }) => {
+	const text_input$ = DOM.select('.new-task').events('input')
+		.map(ev => ev.target.value)
+	const add_task_click$ = DOM.select('.add-task').events('click')
+
+	const new_task = {
+		text$: xs.merge(text_input$, add_task_click$.mapTo('')).startWith(''),
+	}
+
+	const tasks$ = add_task_click$.compose(sampleCombine(text_input$))
+		.map(([_, task]) => task)
+		.fold((acc, x) => ([...acc, x]), ponies)
+
 	return {
-		DOM: view(),
+		DOM: view(tasks$, new_task.text$),
 		title$: xs.of('Ponies'),
 	}
-})(sources)
+}
+//)(sources)
